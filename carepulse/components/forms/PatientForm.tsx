609ce421acffa5c8d537b19fcd891362621fb7eb
aryannaik225 <aryann2203@gmail.com>
2024-08-25 11:23:37 +0530

    frontend of form completed

--- carepulse/components/forms/PatientForm.tsx
+++ carepulse/components/forms/PatientForm.tsx
@@ -8,6 +8,11 @@ import { Form } from "@/components/ui/form"
 import CustomFormField from "../ui/CustomFormField"
 import UserIcon from "@/public/assets/icons/user.svg"
 import EmailIcon from "@/public/assets/icons/email.svg"
+import SubmitButton from "../SubmitButton"
+import { useState } from "react"
+import { UserFormValidation } from "@/lib/validation"
+import { create } from "domain"
+import { useRouter } from "next/navigation"
 
 export enum FormFieldType {
     INPUT = "input",
@@ -18,24 +23,33 @@ export enum FormFieldType {
     SELECT = 'select',
     SKELETON = 'skeleton'
 }
-
- 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
  
 const PatientForm = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: "",
     },
   })
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
+  async function onSubmit({ name, email, phone} : z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+    
+    try {
+      //Below three lines are the lines for database connectivity
+      // const userData = { name, email, phone }
+
+      // const user = await createUser(userData);
+
+      // if(user) router.push(`/patients/${user.$id}/register`);
+    } catch (error) {
+      console.log(error);
+    }
   }
   return (
     <Form {...form}>
@@ -74,7 +88,7 @@ const PatientForm = () => {
         />
 
 
-        <Button type="submit">Submit</Button>
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
   )
